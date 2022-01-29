89e5b35b86b6ff4a01651e59a8a47d8bca5514ce
Swapnil <swapnilvernekar04@gmail.com>
2022-01-29 21:28:35 +0530

    Initial Commit

---
+++ 02-frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3m2703erGTLfDv5d7',
+        issuer: 'https://dev-15204862.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
